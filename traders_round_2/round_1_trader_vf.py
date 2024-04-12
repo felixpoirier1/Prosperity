1c434feaf64b09e1106e755cc293be8e6f11cb92
Giovanni Longo <gvlongo@uchicago.edu>
2024-04-12 12:29:38 -0500

    New trader files here

---
+++ traders_round_2/round_1_trader_vf.py
@@ -0,0 +1,264 @@
+import json
+import jsonpickle
+from datamodel import Listing, Observation, Order, OrderDepth, ProsperityEncoder, Symbol, Trade, TradingState
+from typing import Any, List
+from collections import defaultdict
+import numpy as np
+import copy
+import operator
+
+
+class Logger:
+    def __init__(self) -> None:
+        self.logs = ""
+
+    def print(self, *objects: Any, sep: str = " ", end: str = "\n") -> None:
+        self.logs += sep.join(map(str, objects)) + end
+
+    def flush(self, state: TradingState, orders: dict[Symbol, list[Order]], conversions: int, trader_data: str) -> None:
+        print(json.dumps([
+            self.compress_state(state),
+            self.compress_orders(orders),
+            conversions,
+            trader_data,
+            self.logs,
+        ], cls=ProsperityEncoder, separators=(",", ":")))
+
+        self.logs = ""
+
+    def compress_state(self, state: TradingState) -> list[Any]:
+        return [
+            state.timestamp,
+            state.traderData,
+            self.compress_listings(state.listings),
+            self.compress_order_depths(state.order_depths),
+            self.compress_trades(state.own_trades),
+            self.compress_trades(state.market_trades),
+            state.position,
+            self.compress_observations(state.observations),
+        ]
+
+    def compress_listings(self, listings: dict[Symbol, Listing]) -> list[list[Any]]:
+        compressed = []
+        for listing in listings.values():
+            compressed.append([listing["symbol"], listing["product"], listing["denomination"]])
+
+        return compressed
+
+    def compress_order_depths(self, order_depths: dict[Symbol, OrderDepth]) -> dict[Symbol, list[Any]]:
+        compressed = {}
+        for symbol, order_depth in order_depths.items():
+            compressed[symbol] = [order_depth.buy_orders, order_depth.sell_orders]
+
+        return compressed
+
+    def compress_trades(self, trades: dict[Symbol, list[Trade]]) -> list[list[Any]]:
+        compressed = []
+        for arr in trades.values():
+            for trade in arr:
+                compressed.append([
+                    trade.symbol,
+                    trade.price,
+                    trade.quantity,
+                    trade.buyer,
+                    trade.seller,
+                    trade.timestamp,
+                ])
+
+        return compressed
+
+    def compress_observations(self, observations: Observation) -> list[Any]:
+        conversion_observations = {}
+        for product, observation in observations.conversionObservations.items():
+            conversion_observations[product] = [
+                observation.bidPrice,
+                observation.askPrice,
+                observation.transportFees,
+                observation.exportTariff,
+                observation.importTariff,
+                observation.sunlight,
+                observation.humidity,
+            ]
+
+        return [observations.plainValueObservations, conversion_observations]
+
+    def compress_orders(self, orders: dict[Symbol, list[Order]]) -> list[list[Any]]:
+        compressed = []
+        for arr in orders.values():
+            for order in arr:
+                compressed.append([order.symbol, order.price, order.quantity])
+
+        return compressed
+
+logger = Logger()
+
+class Trader:
+    def __init__(self) -> None:
+        self.position = copy.deepcopy({'STARFRUIT':0, 'AMETHYSTS':0})
+
+        self.cpnl = defaultdict(lambda : 0)
+        self.sf_cache = []
+        self.POSITION_LIMIT = {'STARFRUIT':20, 'AMETHYSTS':20}
+        self.sf_params = [0.08442609, 0.18264657, 0.7329293]
+
+    def get_deepest_prices(self, order_depth):
+        best_sell_pr = sorted(order_depth.sell_orders.items())[-1][0]
+        best_buy_pr = sorted(order_depth.buy_orders.items())[0][0]
+
+        return best_sell_pr, best_buy_pr
+
+    def get_price_condition(self, is_sell, price, acc_price, product, comp_op):
+        first_val = comp_op(price, acc_price)
+        if is_sell:
+            if product == 'STARFRUIT':
+                acc_price += 1
+            return (first_val or ((self.position[product]<0) and (price == acc_price))) 
+        else:
+           if product == 'STARFRUIT':
+                price += 1
+           return (first_val or ((self.position[product]>0) and (price == acc_price))) 
+
+    def liquity_taking(self, orders_dict, acc_price, is_sell, product, comp_op):
+        cpos = self.position[product]
+        pos_limit = self.POSITION_LIMIT[product]
+        orders = []
+
+        for price, vol in orders_dict.items():
+            price_condition = self.get_price_condition(is_sell, price, acc_price, product, comp_op)
+            position_condition = cpos < pos_limit if is_sell else cpos > -pos_limit
+            if price_condition and position_condition:
+                order_for = min(-vol, pos_limit-cpos) if is_sell else max(-vol, -pos_limit-cpos)
+                cpos += order_for
+                orders.append(Order(product, price, order_for))
+
+        return orders, cpos
+
+    def compute_orders_amethysts(self, product, order_depth, acc_bid, acc_ask):
+        orders: list[Order] = []
+        pos_lim = self.POSITION_LIMIT[product]
+
+        order_s_liq, bcpos = self.liquity_taking(order_depth.sell_orders, acc_bid, True, product, operator.lt)
+        order_b_liq, acpos = self.liquity_taking(order_depth.buy_orders, acc_ask, False, product, operator.gt)
+
+        # Market making prices
+        if 9998 in order_depth.buy_orders:
+            price_bid = 9998
+        elif 9996 in order_depth.buy_orders:
+            price_bid = 9997
+        else:
+            price_bid = 9996
+
+        if 10_002 in order_depth.sell_orders:
+            price_ask = 10_002
+        elif 10_004 in order_depth.sell_orders:
+            price_ask = 10_003
+        else:
+            price_ask = 10_004
+
+        orders += order_s_liq
+
+        # if bcpos <= -15:
+        #    price_bid += 1
+
+        #if cpos == -pos_lim:
+        #    orders.append(Order(product, 10_002, 1))
+        #    cpos += 1
+
+        # Market making bid orders
+        if bcpos < pos_lim:
+            orders.append(Order(product, price_bid, pos_lim-bcpos))
+        
+        orders += order_b_liq
+
+        #if acpos >= 15:
+        #    price_ask -= 1
+
+        #if cpos == pos_lim:
+        #    orders.append(Order(product, 9998, -1))
+        #    cpos -= 1
+
+        # Market making ask orders
+        if acpos > -pos_lim:
+            orders.append(Order(product, price_ask, -pos_lim-acpos))
+
+        return orders
+
+    def compute_starfruit_orders(self, product, order_depth, next_mid):
+        orders: list[Order] = []
+        lim = self.POSITION_LIMIT[product]
+
+        best_sell_pr, best_buy_pr = self.get_deepest_prices(order_depth)
+
+        if len(self.sf_cache) == len(self.sf_params):
+            bid_pr = min(best_buy_pr+1, next_mid-1)
+            sell_pr = max(best_sell_pr-1, next_mid+1)
+        else:
+            bid_pr = best_buy_pr+1
+            sell_pr = best_sell_pr-1
+
+        order_s_liq, bcpos = self.liquity_taking(order_depth.sell_orders, next_mid-1, True, product, operator.le)
+        orders += order_s_liq
+
+        if bcpos < lim:
+            orders.append(Order(product, bid_pr, lim - bcpos))
+
+        if not next_mid:
+            next_mid = 1E8
+        
+        order_b_liq, acpos = self.liquity_taking(order_depth.buy_orders, next_mid+1, False, product, operator.ge)
+        orders += order_b_liq
+
+        if acpos > -lim:
+            orders.append(Order(product, sell_pr, -lim-acpos))
+
+        return orders
+
+    def deserializeJson(self, json_string):
+        if json_string == "":
+            logger.print("Empty trader data")
+            return
+        state_dict = jsonpickle.decode(json_string)
+        for key, value in state_dict.items():
+            # logger.print(key, value)
+            setattr(self, key, value)
+    
+    def serializeJson(self):
+        return jsonpickle.encode(self.__dict__)
+
+    def run(self, state: TradingState) -> tuple[dict[Symbol, list[Order]], int, str]:
+        try:
+            self.deserializeJson(state.traderData)
+        except:
+            logger.print("Error in deserializing trader data")
+
+        result = {}
+
+        for key, val in state.position.items():
+            self.position[key] = val
+
+        # To be changed later
+        conversions = 0
+        trader_data = state.traderData
+
+        if len(self.sf_cache) == len(self.sf_params):
+            self.sf_cache.pop(0)
+
+        bs_starfruit, bb_starfruit = self.get_deepest_prices(state.order_depths['STARFRUIT'])
+
+        self.sf_cache.append((bs_starfruit+bb_starfruit)/2)
+    
+        next_price = 0
+
+        if len(self.sf_cache) == len(self.sf_params):
+            next_price = int((np.array(self.sf_cache) * np.array(self.sf_params)).sum())
+
+        for product in state.order_depths:
+            order_depth = state.order_depths[product]
+            if product == 'AMETHYSTS':
+                result[product] = self.compute_orders_amethysts(product, order_depth, 10_000, 10_000)
+            elif product == 'STARFRUIT':
+                result[product] = self.compute_starfruit_orders(product, order_depth, next_price)
+
+        trader_data = self.serializeJson()
+        logger.flush(state, result, conversions, trader_data)
+        return result, conversions, trader_data
