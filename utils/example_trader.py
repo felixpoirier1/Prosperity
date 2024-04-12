8336397de4d1febc9666bb02b70b4882ea06d5aa
felixpoirier1 <felixpoirier2001@gmail.com>
2024-04-12 01:45:31 -0500

    standard commit

--- utils/example_trader.py
+++ utils/example_trader.py
@@ -2,13 +2,9 @@ import json
 import jsonpickle
 from datamodel import Listing, Observation, Order, OrderDepth, ProsperityEncoder, Symbol, Trade, TradingState
 from typing import Any, List
-import collections
 from collections import defaultdict
-import numpy as np
 import copy
-import operator
-import time
-from functools import wraps
+
 
 class Logger:
     def __init__(self) -> None:
@@ -103,100 +99,16 @@ def def_value():
 
 
 class Trader:
-    position = copy.deepcopy(empty_dict)
-    volume_traded = copy.deepcopy(empty_dict)
+    def __init__(self):
+        self.position = copy.deepcopy(empty_dict)
+        self.volume_traded = copy.deepcopy(empty_dict)
 
-    person_position = defaultdict(def_value)
-    person_actvalof_position = defaultdict(def_value)
+        self.person_position = defaultdict(def_value)
+        self.person_actvalof_position = defaultdict(def_value)
 
-    cpnl = defaultdict(lambda : 0)
-    sf_cache = []
-    POSITION_LIMIT = {'STARFRUIT':20, 'AMETHYSTS':20} 
-    sf_params = [4.4817, -0.0187, 0.0455,  0.1632,  0.8091]
-
-    def get_deepest_prices(self, order_depth):
-        best_sell_pr = sorted(order_depth.sell_orders.items())[-1][0]
-        best_buy_pr = sorted(order_depth.buy_orders.items())[0][0]
-
-        return best_sell_pr, best_buy_pr
-
-    def get_price_condition(self, is_sell, price, acc_price, product, comp_op):
-        first_val = comp_op(price, acc_price)
-        if is_sell:
-            if product == 'STARFRUIT':
-                acc_price += 1
-            return (first_val or ((self.position[product]<0) and (price == acc_price))) 
-        else:
-           if product == 'STARFRUIT':
-                price += 1
-           return (first_val or ((self.position[product]>0) and (price == acc_price))) 
-
-    def liquity_taking(self, orders_dict, acc_price, is_sell, product, comp_op):
-        cpos = self.position[product]
-        pos_limit = self.POSITION_LIMIT[product]
-        orders = []
-
-        for price, vol in orders_dict.items():
-            price_condition = self.get_price_condition(is_sell, price, acc_price, product, comp_op)
-            position_condition = cpos < pos_limit if is_sell else cpos > -pos_limit
-            if price_condition and position_condition:
-                order_for = min(-vol, pos_limit-cpos) if is_sell else max(-vol, -pos_limit-cpos)
-                cpos += order_for
-                assert(order_for >=0 if is_sell else order_for <= 0)
-                orders.append(Order(product, price, order_for))
-
-        return orders, cpos
-
-    def compute_orders_amethysts(self, product, order_depth, acc_bid, acc_ask):
-        orders: list[Order] = []
-        pos_lim = self.POSITION_LIMIT[product]
-
-        order_s_liq, cpos = self.liquity_taking(order_depth.sell_orders, acc_bid, True, product, operator.lt)
-        orders += order_s_liq
-
-        # Market making prices
-        price_bid = 9997 if 9996 in order_depth.buy_orders else 9996
-        price_ask = 10_003 if 10_004 in order_depth.sell_orders else 10_004
-
-        # Market making bid orders
-        if cpos < pos_lim:
-            orders.append(Order(product, price_bid, pos_lim-cpos))
-        
-        order_b_liq, cpos = self.liquity_taking(order_depth.buy_orders, acc_ask, False, product, operator.gt)
-        orders += order_b_liq
-
-        # Market making ask orders
-        if cpos > -pos_lim:
-            orders.append(Order(product, price_ask, -pos_lim-cpos))
-
-        return orders
-
-    def compute_starfruit_orders(self, product, order_depth, acc_bid, acc_ask):
-        orders: list[Order] = []
-        lim = self.POSITION_LIMIT[product]
-
-        best_sell_pr, best_buy_pr = self.get_deepest_prices(order_depth)
-
-        if len(self.sf_cache) == (len(self.sf_params) - 1):
-            bid_pr = min(best_buy_pr+1, acc_bid)
-            sell_pr = max(best_sell_pr-1, acc_ask)
-        else:
-            bid_pr = best_buy_pr+1
-            sell_pr = best_sell_pr-1
-
-        order_s_liq, cpos = self.liquity_taking(order_depth.sell_orders, acc_bid, True, product, operator.le)
-        orders += order_s_liq
-
-        if cpos < lim:
-            orders.append(Order(product, bid_pr, lim - cpos))
-        
-        order_b_liq, cpos = self.liquity_taking(order_depth.buy_orders, acc_ask, False, product, operator.ge)
-        orders += order_b_liq
-
-        if cpos > -lim:
-            orders.append(Order(product, sell_pr, -lim-cpos))
-
-        return orders
+        self.cpnl = defaultdict(lambda : 0)
+        self.sf_cache = []
+        self.POSITION_LIMIT = {'STARFRUIT':20, 'AMETHYSTS':20} 
     
     def deserializeJson(self, json_string):
         if json_string == "":
@@ -226,31 +138,6 @@ class Trader:
         # To be changed later
         conversions = 0
         trader_data = ""
- 
-        if len(self.sf_cache) == (len(self.sf_params) - 1):
-            self.sf_cache.pop(0)
-
-        bs_starfruit, bb_starfruit = self.get_deepest_prices(state.order_depths['STARFRUIT'])
-
-        self.sf_cache.append((bs_starfruit+bb_starfruit)/2)
-
-        INF = 1e9
-    
-        starfruit_lb = -INF
-        starfruit_ub = INF
-
-        if len(self.sf_cache) == (len(self.sf_params) - 1):
-            next_price = int((np.array(self.sf_cache) * np.array(self.sf_params[1:])).sum() + self.sf_params[0])
-
-            starfruit_lb = next_price-1
-            starfruit_ub = next_price+1
-
-        for product in state.order_depths:
-            order_depth = state.order_depths[product]
-            if product == 'AMETHYSTS':
-                result[product] = self.compute_orders_amethysts(product, order_depth, 10_000, 10_000)
-            elif product == 'STARFRUIT':
-                result[product] = self.compute_starfruit_orders(product, order_depth, starfruit_lb, starfruit_ub)
         
         trader_data = self.serializeJson()
         logger.flush(state, result, conversions, trader_data)
