fcc5dce0e3be138eb3c9adc17657a7533b8ab80d
Raafay Uqaily <mr.uqaily2000@gmail.com>
2024-04-17 08:33:36 -0500

    benchmark r3 added

---
+++ traders_round_3/cardinals_r3.py
@@ -0,0 +1,430 @@
+## THIS IS THE CARDINALS CODE (BASKET FUNCTION ONLY!!) INTEGRATED WITH OUR TRADER FILE
+
+import json
+import jsonpickle
+from datamodel import Listing, Observation, Order, OrderDepth, ProsperityEncoder, Symbol, Trade, TradingState
+from typing import Any, List, Dict, Tuple
+from collections import defaultdict, OrderedDict
+import numpy as np
+import copy
+import operator
+
+
+class Logger:
+    def __init__(self) -> None:
+        self.logs = ""
+
+    def print(self, *objects: Any, sep: str = " ", end: str = "\n") -> None:
+        self.logs += sep.join(map(str, objects)) + end
+
+    def flush(self, state: TradingState, orders: dict[Symbol, list[Order]], conversions: int, trader_data: str) -> None:
+        print(json.dumps([
+            self.compress_state(state),
+            self.compress_orders(orders),
+            conversions,
+            trader_data,
+            self.logs,
+        ], cls=ProsperityEncoder, separators=(",", ":")))
+
+        self.logs = ""
+
+    def compress_state(self, state: TradingState) -> list[Any]:
+        return [
+            state.timestamp,
+            state.traderData,
+            self.compress_listings(state.listings),
+            self.compress_order_depths(state.order_depths),
+            self.compress_trades(state.own_trades),
+            self.compress_trades(state.market_trades),
+            state.position,
+            self.compress_observations(state.observations),
+        ]
+
+    def compress_listings(self, listings: dict[Symbol, Listing]) -> list[list[Any]]:
+        compressed = []
+        for listing in listings.values():
+            compressed.append([listing["symbol"], listing["product"], listing["denomination"]])
+
+        return compressed
+
+    def compress_order_depths(self, order_depths: dict[Symbol, OrderDepth]) -> dict[Symbol, list[Any]]:
+        compressed = {}
+        for symbol, order_depth in order_depths.items():
+            compressed[symbol] = [order_depth.buy_orders, order_depth.sell_orders]
+
+        return compressed
+
+    def compress_trades(self, trades: dict[Symbol, list[Trade]]) -> list[list[Any]]:
+        compressed = []
+        for arr in trades.values():
+            for trade in arr:
+                compressed.append([
+                    trade.symbol,
+                    trade.price,
+                    trade.quantity,
+                    trade.buyer,
+                    trade.seller,
+                    trade.timestamp,
+                ])
+
+        return compressed
+
+    def compress_observations(self, observations: Observation) -> list[Any]:
+        conversion_observations = {}
+        for product, observation in observations.conversionObservations.items():
+            conversion_observations[product] = [
+                observation.bidPrice,
+                observation.askPrice,
+                observation.transportFees,
+                observation.exportTariff,
+                observation.importTariff,
+                observation.sunlight,
+                observation.humidity,
+            ]
+
+        return [observations.plainValueObservations, conversion_observations]
+
+    def compress_orders(self, orders: dict[Symbol, list[Order]]) -> list[list[Any]]:
+        compressed = []
+        for arr in orders.values():
+            for order in arr:
+                compressed.append([order.symbol, order.price, order.quantity])
+
+        return compressed
+
+logger = Logger()
+
+class Trader:
+    def __init__(self) -> None:
+        self.position = {'STARFRUIT':0, 'AMETHYSTS':0, 'ORCHIDS':0, 'CHOCOLATE': 0, 'ROSES': 0, "GIFT_BASKET": 0 , 'STRAWBERRIES': 0}
+
+        self.cpnl = defaultdict(lambda : 0)
+        self.sf_cache = []
+        self.POSITION_LIMIT = {'STARFRUIT':20, 'AMETHYSTS':20, 'ORCHIDS':100, 'CHOCOLATE': 250, 'ROSES': 60, "GIFT_BASKET": 60 , 'STRAWBERRIES': 350}
+        self.sf_params = [0.08442609, 0.18264657, 0.7329293]
+
+        # orchids
+        self.orch_ask = 0
+        self.orch_bid = 0
+        self.arb = None
+
+        # etf
+        self.etf_norm_const = 1000 #the lower the value, the more liquidity taking is prioritized over market making
+        self.spread_std = 75
+        self.basket_std = 330
+
+        self.cont_buy_basket_unfill = 0
+        self.cont_sell_basket_unfill = 0
+        self.do_bask = 0
+        self.basket_buy_sig = 0
+        self.basket_sell_sig = 0
+        self.rose_pos = 0
+        self.rose_neg = 0
+
+    def get_deepest_prices(self, order_depth):
+        best_sell_pr = sorted(order_depth.sell_orders.items())[-1][0]
+        best_buy_pr = sorted(order_depth.buy_orders.items())[0][0]
+
+        return best_sell_pr, best_buy_pr
+
+    def get_price_condition(self, is_sell, price, acc_price, product, comp_op):
+        first_val = comp_op(price, acc_price)
+        if is_sell:
+            if product == 'STARFRUIT':
+                acc_price += 1
+            return (first_val or ((self.position[product]<0) and (price == acc_price))) 
+        else:
+           if product == 'STARFRUIT':
+                price += 1
+           return (first_val or ((self.position[product]>0) and (price == acc_price))) 
+
+    def liquity_taking(self, orders_dict, acc_price, is_sell, product, comp_op):
+        cpos = self.position[product]
+        pos_limit = self.POSITION_LIMIT[product]
+        orders = []
+
+        for price, vol in orders_dict.items():
+            price_condition = self.get_price_condition(is_sell, price, acc_price, product, comp_op)
+            position_condition = cpos < pos_limit if is_sell else cpos > -pos_limit
+            if price_condition and position_condition:
+                order_for = min(-vol, pos_limit-cpos) if is_sell else max(-vol, -pos_limit-cpos)
+                cpos += order_for
+                orders.append(Order(product, price, order_for))
+
+        return orders, cpos
+
+    def compute_orders_amethysts(self, product, order_depth, acc_bid, acc_ask):
+        orders: list[Order] = []
+        pos_lim = self.POSITION_LIMIT[product]
+
+        order_s_liq, bcpos = self.liquity_taking(order_depth.sell_orders, acc_bid, True, product, operator.lt)
+        order_b_liq, acpos = self.liquity_taking(order_depth.buy_orders, acc_ask, False, product, operator.gt)
+
+        # Market making prices
+        if 9998 in order_depth.buy_orders:
+            price_bid = 9998
+        elif 9996 in order_depth.buy_orders:
+            price_bid = 9997
+        else:
+            price_bid = 9996
+
+        if 10_002 in order_depth.sell_orders:
+            price_ask = 10_002
+        elif 10_004 in order_depth.sell_orders:
+            price_ask = 10_003
+        else:
+            price_ask = 10_004
+
+        orders += order_s_liq
+
+        # Market making bid orders
+        if bcpos < pos_lim:
+            orders.append(Order(product, price_bid, pos_lim-bcpos))
+        
+        orders += order_b_liq
+
+        # Market making ask orders
+        if acpos > -pos_lim:
+            orders.append(Order(product, price_ask, -pos_lim-acpos))
+
+        return orders
+
+    def compute_starfruit_orders(self, product, order_depth, next_mid):
+        orders: list[Order] = []
+        lim = self.POSITION_LIMIT[product]
+
+        best_sell_pr, best_buy_pr = self.get_deepest_prices(order_depth)
+
+        if len(self.sf_cache) == len(self.sf_params):
+            bid_pr = min(best_buy_pr+1, next_mid-1)
+            sell_pr = max(best_sell_pr-1, next_mid+1)
+        else:
+            bid_pr = best_buy_pr+1
+            sell_pr = best_sell_pr-1
+
+        order_s_liq, bcpos = self.liquity_taking(order_depth.sell_orders, next_mid-1, True, product, operator.le)
+        orders += order_s_liq
+
+        if bcpos < lim:
+            orders.append(Order(product, bid_pr, lim - bcpos))
+
+        if not next_mid:
+            next_mid = 1E8
+        
+        order_b_liq, acpos = self.liquity_taking(order_depth.buy_orders, next_mid+1, False, product, operator.ge)
+        orders += order_b_liq
+
+        if acpos > -lim:
+            orders.append(Order(product, sell_pr, -lim-acpos))
+
+        return orders
+
+    def get_orchid_prices(self, added_val, top_ask, top_buy, second_ask):
+        mid_price = int((top_buy+top_ask)/2)
+        if added_val <= 1.168099856680999:
+            self.arb = 'low'
+        elif added_val >= 2.168099856680999:
+            self.arb = 'high'
+        else:
+            self.arb = 'mid'
+
+        if top_ask - top_buy > 4:
+            # Mean value of import - fees
+            if self.arb == 'low':
+                ask_pr = mid_price+1
+            elif self.arb == 'high':
+                ask_pr = mid_price-1
+            else:
+                ask_pr = mid_price
+        else:
+            if top_ask-top_buy == 4:
+                if self.arb == 'high':
+                    ask_pr = top_ask-2
+                elif self.arb == 'low':
+                    ask_pr = top_ask
+                else:
+                    ask_pr = top_ask-1
+            else:
+                if second_ask-top_ask == 1:
+                    ask_pr = top_ask-1
+                else:
+                    if self.arb == 'low':
+                        ask_pr = top_ask + 1
+                    else:
+                        ask_pr = top_ask
+            
+        return ask_pr
+    
+    def compute_orchids_orders(self, product, order_depth, observation):
+        orders: list[Order] = []
+        pos_lim = self.POSITION_LIMIT[product]
+        apos = self.position[product]
+        bpos = self.position[product]
+        convsersions = 0
+
+        ap = observation.askPrice
+        imp = observation.importTariff
+        fee = observation.transportFees
+
+        top_ask, top_buy = sorted(order_depth.sell_orders.items())[0][0], sorted(order_depth.buy_orders.items())[-1][0]
+        second_ask = sorted(order_depth.sell_orders.items())[0][0]
+        ask_pr = self.get_orchid_prices(-imp-fee, top_ask, top_buy, second_ask)
+
+        ask_arb_val = max(ask_pr, self.orch_ask)
+
+        arb_val = ask_arb_val-ap-imp-fee
+
+        if apos < 0:
+            if arb_val > 0:
+                convsersions -= bpos
+                apos += convsersions
+                bpos -= convsersions
+            else:
+                if self.arb == 'low':
+                    orders.append(Order(product, max(self.orch_ask-1, int((top_ask+top_buy)/2))+1, -bpos))
+                    bpos -= bpos
+
+        if apos > -pos_lim:
+            orders.append(Order(product, ask_pr, -pos_lim-apos))
+            self.orch_ask = ask_pr
+
+        return orders, convsersions
+        
+    def compute_orders_basket(self, order_depth: Dict[Symbol, OrderDepth]) -> Dict[Symbol, list[Order]]:
+        orders = {'STRAWBERRIES' : [], 'CHOCOLATE': [], 'ROSES' : [], 'GIFT_BASKET' : []}
+        prods = ['STRAWBERRIES', 'CHOCOLATE', 'ROSES', 'GIFT_BASKET']
+        osell, obuy, best_sell, best_buy, worst_sell, worst_buy, mid_price, vol_buy, vol_sell = {}, {}, {}, {}, {}, {}, {}, {}, {}
+
+        for p in prods:
+            osell[p] = OrderedDict(sorted(order_depth[p].sell_orders.items()))
+            obuy[p] = OrderedDict(sorted(order_depth[p].buy_orders.items(), reverse=True))
+
+            best_sell[p] = next(iter(osell[p]))
+            best_buy[p] = next(iter(obuy[p]))
+
+            worst_sell[p] = next(reversed(osell[p]))
+            worst_buy[p] = next(reversed(obuy[p]))
+
+            mid_price[p] = (best_sell[p] + best_buy[p]) / 2
+            vol_buy[p], vol_sell[p] = 0, 0
+            for price, vol in obuy[p].items():
+                vol_buy[p] += vol 
+                if vol_buy[p] >= self.POSITION_LIMIT[p] / 10:
+                    break
+            for price, vol in osell[p].items():
+                vol_sell[p] += -vol 
+                if vol_sell[p] >= self.POSITION_LIMIT[p] / 10:
+                    break
+
+        res_buy = mid_price['GIFT_BASKET'] - mid_price['STRAWBERRIES'] * 6 - mid_price['CHOCOLATE'] * 4 - mid_price['ROSES'] - 385
+        res_sell = mid_price['GIFT_BASKET'] - mid_price['STRAWBERRIES'] * 6 - mid_price['CHOCOLATE'] * 4 - mid_price['ROSES'] - 385
+
+        trade_at = self.basket_std * 0.5
+        close_at = self.basket_std * (-1000)
+
+        gb_pos = self.position['GIFT_BASKET']
+        gb_neg = self.position['GIFT_BASKET']
+        
+        rose_pos = self.position['ROSES']
+        rose_neg = self.position['ROSES']
+        
+        basket_buy_sig = 0
+        basket_sell_sig = 0
+
+        if self.position['GIFT_BASKET'] == self.POSITION_LIMIT['GIFT_BASKET']:
+            self.cont_buy_basket_unfill = 0
+        if self.position['GIFT_BASKET'] == -self.POSITION_LIMIT['GIFT_BASKET']:
+            self.cont_sell_basket_unfill = 0
+        
+        do_bask = 0
+
+        if res_sell > trade_at:
+            vol = self.position['GIFT_BASKET'] + self.POSITION_LIMIT['GIFT_BASKET']
+            self.cont_buy_basket_unfill = 0  # no need to buy rn
+            assert(vol >= 0)
+            if vol > 0:
+                do_bask = 1
+                basket_sell_sig = 1
+                orders['GIFT_BASKET'].append(Order('GIFT_BASKET', worst_buy['GIFT_BASKET'], -vol)) 
+                self.cont_sell_basket_unfill += 2
+                gb_neg -= vol
+                #rose_pos += vol
+        elif res_buy < -trade_at:
+            vol = self.POSITION_LIMIT['GIFT_BASKET'] - self.position['GIFT_BASKET']
+            self.cont_sell_basket_unfill = 0  # no need to sell rn
+            assert(vol >= 0)
+            if vol > 0:
+                do_bask = 1
+                basket_buy_sig = 1
+                orders['GIFT_BASKET'].append(Order('GIFT_BASKET', worst_sell['GIFT_BASKET'], vol))
+                self.cont_buy_basket_unfill += 2
+                gb_pos += vol
+                
+        return orders
+    
+    def compute_etf_orders(self, order_depths: Dict[Symbol, OrderDepth], etf: OrderDepth, etf_components: Dict[Symbol, OrderDepth]) -> dict[Symbol, list[Order]]:
+        etf_orders = self.compute_orders_basket(order_depths)
+
+        return etf_orders
+
+    def deserializeJson(self, json_string):
+        if json_string == "":
+            return
+        state_dict = jsonpickle.decode(json_string)
+        for key, value in state_dict.items():
+            setattr(self, key, value)
+    
+    def serializeJson(self):
+        return jsonpickle.encode(self.__dict__)
+
+    def run(self, state: TradingState) -> tuple[dict[Symbol, list[Order]], int, str]:
+        try:
+            self.deserializeJson(state.traderData)
+        except:
+            logger.print("Error in deserializing trader data")
+
+        result = {}
+
+        for key, val in state.position.items():
+            self.position[key] = val
+
+        trader_data = state.traderData
+        conversion_observation = state.observations.conversionObservations
+        orchid_observation = conversion_observation['ORCHIDS']
+        etf_components: Dict[Symbol, OrderDepth] = {}
+        etf: OrderDepth = None
+
+        if len(self.sf_cache) == len(self.sf_params):
+            self.sf_cache.pop(0)
+
+        bs_starfruit, bb_starfruit = self.get_deepest_prices(state.order_depths['STARFRUIT'])
+
+        self.sf_cache.append((bs_starfruit+bb_starfruit)/2)
+    
+        next_price = 0
+
+        if len(self.sf_cache) == len(self.sf_params):
+            next_price = int((np.array(self.sf_cache) * np.array(self.sf_params)).sum())
+
+        for product in state.order_depths:
+            order_depth = state.order_depths[product]
+            if product == 'AMETHYSTS':
+                result[product] = self.compute_orders_amethysts(product, order_depth, 10_000, 10_000)
+            elif product == 'STARFRUIT':
+                result[product] = self.compute_starfruit_orders(product, order_depth, next_price)
+            elif product == 'ORCHIDS':
+                orchid_orders, conversions = self.compute_orchids_orders(product, order_depth, orchid_observation)
+                result[product] = orchid_orders
+            elif product == 'GIFT_BASKET':
+                etf = order_depth
+            elif product == 'CHOCOLATE' or product == 'ROSES' or product == 'STRAWBERRIES':
+                etf_components[product] = order_depth
+
+        etf_orders = self.compute_etf_orders(state.order_depths, etf, etf_components)
+        for prod, ords in etf_orders.items():
+            if ords:
+                result[prod] = ords
+
+        trader_data = self.serializeJson()
+        logger.flush(state, result, conversions, trader_data)
+        return result, conversions, trader_data
